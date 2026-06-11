44792052955c16f30f76f73115fa9afdf5d27253
CheefLofter <ravneetgrewal1000@gmail.com>
2026-06-11 22:58:30 +0530

    server ssetup compleate error to fix

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
-
 from flask import request
-
 from src.encryption import encrypt_zip
-#import base64
+import base64
+from flask_cloudflared import run_with_cloudflared, get_cloudflared_url
 
-
+storage = {"blob": None, "filename": None}
 
 app = Flask(__name__)
+
+# Automatically starts a TryCloudflare tunnel
+run_with_cloudflared(app)
 
 @app.route("/")
 @app.route("/send")
@@ -24,9 +26,27 @@ def upload_file():
     raw = file.read()
 
     blob, key_fragment = encrypt_zip(raw)
+
+    storage["blob"] = blob
+    storage["filename"] = file.filename
+
     #print("Key Fragment:", key_fragment)
     #blob_b64 = base64.urlsafe_b64encode(blob).decode()
-    return key_fragment, 200
+    url = get_cloudflared_url()
+    return f"Hello World! Accessible at: {url}/download#key={key_fragment}"
+
+
+
+@app.route("/download")
+def download_file():
+    blob = storage["blob"]
+    filename = storage["filename"]
+    if not blob:
+        return "No file available", 404
+
+    blob_b64 = base64.b64encode(blob).decode()
+    return render_template("receiver.html", blob_b64=blob_b64,filename=filename)
+
 
 
    
